9641a5d445adc0faea95e41a93cc86068f55ef91
ristica <alri@chello.at>
2024-02-09 16:00:28 +0100

    delegatecommand added

---
+++ Chrome.ViewModels/Commands/Base/DelegateCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Chrome.ViewModels.Commands.Base;
+
+public abstract class DelegateCommand
+    (Action<object> action, Func<object, bool>? canExecute = null)
+{
+    public abstract void Execute(object parameter);
+    public abstract bool CanExecute(object parameter);
+
+    public virtual void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
