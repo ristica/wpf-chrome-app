8b1fa943fb5b9eab3ead03b427bde10ac6e6500c
ristica <alri@chello.at>
2024-02-21 00:23:20 +0100

    mc

--- Chrome.Views/Converters/MenuModelToBorderWidthConverter.cs
+++ Chrome.Views/Converters/MenuModelToBorderWidthConverter.cs
@@ -8,10 +8,7 @@ public class MenuModelToBorderWidthConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value == null) return new Thickness(1, 32, 0, 0);
-
-        return new Thickness((double)value - 1.00, 0, 0, 0);
-        
+        return value == null ? new Thickness(1, 32, 0, 0) : new Thickness((double)value - 1.00, 0, 0, 0);
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
