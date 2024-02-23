ea5262dd535286063355f26c3fbd094612f5dd7f
aleks ristic <al.ri@chello.at>
2024-02-23 22:43:32 +0100

    mc

--- Chrome.Views/Windows/Uebersicht/UbersichtAktion.xaml.cs
+++ Chrome.Views/Windows/Uebersicht/UbersichtAktion.xaml.cs
@@ -3,7 +3,8 @@ using Chrome.Views.Contracts.Uebersicht;
 
 namespace Chrome.Views.Windows.Uebersicht;
 
-public partial class UbersichtAktion : BaseUserControl, IUebersichtAktionUserControl
+public partial class UbersichtAktion 
+    : BaseUserControl, IUebersichtAktionUserControl
 {
     public UbersichtAktion()
     {
